9152ef1ccad15c4862e0475e1a77604fe22e4792
Snigdha Reddy Pulim <snigdhapulim2@gmail.com>
2019-11-10 18:06:18 +0530

    Updates

---
+++ precache-manifest.492b9554188116008c8b018115879195.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f61874c3c1cf480e2bf68e728fb44fb",
+    "url": "/image-search-app/index.html"
+  },
+  {
+    "revision": "b77cb95695b077c86a17",
+    "url": "/image-search-app/static/js/2.7abaf4c1.chunk.js"
+  },
+  {
+    "revision": "265a0dcf66a2c136ec6a",
+    "url": "/image-search-app/static/js/main.bfc5dfa6.chunk.js"
+  },
+  {
+    "revision": "2899a6ceb3ae6c64b443",
+    "url": "/image-search-app/static/js/runtime-main.efdd8c03.js"
+  }
+]);
